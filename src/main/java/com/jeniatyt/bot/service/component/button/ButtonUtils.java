b07427b01ca3d3089d1bf55d5d9d97c09bf1d9c1
Evgeniy Uvarov <evgeniy.uvarov@gridnine.com>
2023-02-25 12:08:33 +0300

    Bot-5

--- src/main/java/com/jeniatyt/bot/service/component/button/ButtonUtils.java
+++ src/main/java/com/jeniatyt/bot/service/component/button/ButtonUtils.java
@@ -1,10 +1,12 @@
 package com.jeniatyt.bot.service.component.button;
 
+import lombok.experimental.UtilityClass;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 
 import java.util.List;
 
+@UtilityClass
 public class ButtonUtils {
     
     public static InlineKeyboardMarkup inlineMarkup(InlineKeyboardButton... buttons) {
